7191d4016e766bef0c95af4f82d8cd96a3adda40
SniffRx <37187657+SniffRx@users.noreply.github.com>
2021-10-11 11:19:59 +0300

    Working
    Output in console all information and output id on web page

--- src/Weather.js
+++ src/Weather.js
@@ -1,26 +1,21 @@
 import React from 'react'
 import axios from 'axios'
 import API from './API'
-
 const CityName = 'Kaluga';
 const ApiID = '297c67b07a1c365c1ba09ed002715c27';
-
 export default class Weather extends React.Component {
-    
     state = {weathers: []}
-
     componentDidMount() {
-        API.get(`weather?q=${CityName}&appid=${ApiID}`)
-        .then(
+        API.get(`weather?q=${CityName}&appid=${ApiID}`).then(
             res => {
                 const weathers = res.data;
-                this.setState({weathers: weathers.conversations});
-                console.log(weathers);
+                this.setState({weathers:weathers});
             })
-            //function (response) {
-        // handle success
-        //console.log(response);
-        }
-    alert(state.weathers)
-        render() {return(<div>{this.state.weathers}</div>)}
+    }
+    render() {
+        return(
+        <div>{this.state.weathers.id}{console.log(this.state.weathers.id)}</div>
+        )
+        
+    }
 }
