da38b0179a6e7892887e5a67938fd4b5d8509a7f
SniffRx <37187657+SniffRx@users.noreply.github.com>
2021-10-11 12:04:11 +0300

    Test multiple massive
    Test multiple massive

--- src/Weather.js
+++ src/Weather.js
@@ -6,15 +6,28 @@ const ApiID = '297c67b07a1c365c1ba09ed002715c27';
 export default class Weather extends React.Component {
     state = {weathers: []}
     componentDidMount() {
-        API.get(`weather?q=${CityName}&appid=${ApiID}`).then(
+        API.get(`weather?q=${CityName}&appid=${ApiID}&units=metric`).then(
             res => {
                 const weathers = res.data;
                 this.setState({weathers:weathers});
             })
     }
+    
     render() {
         return(
-        <div>{this.state.weathers.id}{console.log(this.state.weathers.id)}</div>
+            <div className="container">
+                <div className="panel">
+                    <p>Страна: {this.state.weathers.name}</p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p></p>
+                    <p>Температура {this.state.weathers['main']/*.temp*/}</p>
+                    <p>Скорость ветра {this.state.weathers.wind/*.speed*/}</p>{console.log(this.state.weathers['main']['temp'])}
+                </div>
+            </div>
         )
         
     }
